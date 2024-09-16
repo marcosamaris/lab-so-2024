147d48de41deb08992f4110ce0f1c572644a7dd2
Marcos Amaris <marcos.amaris@gmail.com>
2024-09-16 11:02:31 -0300

    Files about scheduling algorithm exercice

---
+++ sched/comp-bound.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+x=1
+while [ true ]
+do
+  #echo "Welcome $x times"
+  x=$(( $x + 1 ))
+done
---
+++ sched/exercicio1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Digite o PID do processo a ser alterado:"
+read PID
+echo "Digite o tipo de escalonamento (0: SCHED_OTHER, 1: SCHED_FIFO, 2: SCHED_RR, 3: SCHED_BATCH):"
+read POLICY
+
+chrt -p $POLICY $PID
+echo "Política de escalonamento do processo $PID alterada."
